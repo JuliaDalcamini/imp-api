88855e8b0b736b227141182fe305e28144ebca0b
Julia Dalcamini <jvdalcamini@gmail.com>
2024-04-10 20:59:58 -0300

    feat: add artifact model and repository

---
+++ src/main/kotlin/com/julia/imp/artifact/Artifact.kt
@@ -0,0 +1,17 @@
+package com.julia.imp.artifact
+
+import org.bson.codecs.pojo.annotations.BsonId
+import org.bson.types.ObjectId
+import java.time.LocalDateTime
+
+data class Artifact(
+    @BsonId
+    val id: ObjectId,
+    val name: String,
+    val artifactType: String,
+    val creatorId: ObjectId,
+    val status: Boolean,
+    val creationDateTime: LocalDateTime,
+    val conclusionDateTime: LocalDateTime,
+    val priority: String
+)
---
+++ src/main/kotlin/com/julia/imp/artifact/ArtifactRepository.kt
@@ -0,0 +1,15 @@
+package com.julia.imp.artifact
+
+import com.mongodb.kotlin.client.coroutine.MongoDatabase
+
+class ArtifactRepository(private var database: MongoDatabase) {
+
+    suspend fun insertOne(artifact: Artifact): Boolean {
+        val result = database.getCollection<Artifact>(COLLECTION).insertOne(artifact)
+        return result.insertedId != null
+    }
+
+    companion object {
+        private const val COLLECTION = "artifact"
+    }
+}
